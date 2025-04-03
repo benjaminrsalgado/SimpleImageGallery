4fe0efa6b5c2534f79116e242aaf4d11628ebf91
Benjamin Rojo <benjaminrsalgado@gmail.com>
2025-04-03 11:54:02 -0600

    Initial Commit

---
+++ SimpleImageGallery/SimpleImageGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleImageGalleryApp.swift
+//  SimpleImageGallery
+//
+//  Created by Benjamin Rojo on 03/04/25.
+//
+
+import SwiftUI
+
+@main
+struct SimpleImageGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
